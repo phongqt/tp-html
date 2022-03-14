904751cd2f21cdf37f63e66a8db89b428e1b6b06
phong tran <phong.tran@evizi.com>
2022-03-14 22:28:07 +0700

    Responsive, scroll to element

--- js/custom.js
+++ js/custom.js
@@ -264,4 +264,16 @@ jQuery(document).ready(function($) {
             })
         })
     }
+    // Scroll to element
+    $('li.menu-item-object-page a').click(function(event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            var hash = this.hash;
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function(){
+                window.location.hash = hash;
+            });
+        }
+    });
 });
